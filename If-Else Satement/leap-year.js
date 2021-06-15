c2b9d0d308c93187507ce983d90dfe586a75477b
Atharva <69711024+imatharv@users.noreply.github.com>
2021-06-16 00:06:04 +0530

    Delete If-Else Satement directory

--- If-Else Satement/leap-year.js
+++
@@ -1,9 +0,0 @@
-const readline = require("prompt-sync");
-const prompt = readline();
-
-let year = prompt("Enter year: ");
-
-if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-    console.log("Leap year");
-else
-	console.log("Not a leap year");
